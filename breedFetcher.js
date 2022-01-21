308e7772d886fab2fbb739c4d4e4a34a31ce7a9a
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-21 16:49:28 +0000

    Finished implementing cats as a service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,15 @@
 const request = require('request');
+
+const url = "https://apit.thecatapi.com/v1/breeds/search?q=";
+const keyword = process.argv[2];
+
+request(url + keyword, (error, response, body) => {
+  if (error) throw error;
+  const data = JSON.parse(body);
+  if (data[0] !== undefined) {
+    console.log(data[0]);
+  } else {
+    console.log("The requested breed is not found");
+  }
+
+});
